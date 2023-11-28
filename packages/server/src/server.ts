8dc4dbe1d02e0c6c0e9ade9eb1e28e0ad88b3adf
yungblud <killi8n@gmail.com>
2023-11-28 09:30:37 +0900

    feat: api structure

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,4 +1,6 @@
 import Fastify from 'fastify'
+import AutoLoad from '@fastify/autoload'
+import path from 'path'
 
 const fastify = Fastify({
   logger: true,
@@ -34,6 +36,12 @@ fastify.route({
 
 async function main() {
   try {
+    await fastify.register(AutoLoad, {
+      dir: path.resolve(__dirname, './api/routes'),
+      options: {
+        prefix: '/api/v1',
+      },
+    })
     await fastify.listen({ port: 3000 })
   } catch (err) {
     fastify.log.error(err)
