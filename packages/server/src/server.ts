e39d225424a85c857508a83c42a2a03cd4af03f0
yungblud <killi8n@gmail.com>
2023-11-28 09:35:07 +0900

    chore: removed fastify route

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -6,34 +6,6 @@ const fastify = Fastify({
   logger: true,
 })
 
-fastify.route({
-  method: 'GET',
-  url: '/',
-  schema: {
-    // request needs to have a querystring with a `name` parameter
-    querystring: {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-      },
-      required: ['name'],
-    },
-    // the response needs to be an object with an `hello` property of type 'string'
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          hello: { type: 'string' },
-        },
-      },
-    },
-  },
-  preHandler: async (request, reply) => {
-    // E.g. check authentication
-  },
-  handler: async (request, reply) => ({ hello: 'world' }),
-})
-
 async function main() {
   try {
     await fastify.register(AutoLoad, {
