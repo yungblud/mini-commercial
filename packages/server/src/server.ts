50d5a65952b9a37d7f90d0e076698d994c115abb
yungblud <killi8n@gmail.com>
2023-11-28 09:43:22 +0900

    feat: modified nconf configs

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,20 +1,35 @@
 import Fastify from 'fastify'
 import AutoLoad from '@fastify/autoload'
 import path from 'path'
+import nconf from 'nconf'
 
 const fastify = Fastify({
   logger: true,
 })
 
+async function loadSettings() {
+  return new Promise<void>((resolve, reject) => {
+    try {
+      nconf.file({
+        file: path.resolve(__dirname, './config/config.json'),
+      })
+      resolve()
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
 async function main() {
   try {
+    await loadSettings()
     await fastify.register(AutoLoad, {
       dir: path.resolve(__dirname, './api/routes'),
       options: {
         prefix: '/api/v1',
       },
     })
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: nconf.get('port') })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
