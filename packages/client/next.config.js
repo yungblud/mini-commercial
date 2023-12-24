f0483294d6ec69c8e99c2fc620520f07a021bf27
yungblud <killi8n@gmail.com>
2023-12-24 18:31:25 +0900

    feat: modified next.config.js

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
 
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
 }
 
 module.exports = nextConfig
